54e9fbb7695b53c429e26e07835a78d74697b858
NicoRi5 <150852807+NicoRi5@users.noreply.github.com>
2025-01-13 18:17:45 -0500

    Initial commit

---
+++ prisma/seed.js
@@ -0,0 +1,24 @@
+const prisma = require("./index");
+
+const seed = async () => {
+  const userObj = [
+    { name: "Whitney" },
+    { name: "Daniel" },
+    { name: "Erny" },
+    { name: "Tyrese" },
+    { name: "Shanon" },
+    { name: "Gerard" },
+    { name: "Manny" },
+    { name: "Elizabeth" },
+    { name: "Preston" },
+    { name: "Yolanda" },
+  ];
+  await prisma.employee.createMany({ data: userObj });
+};
+seed()
+  .then(async () => await prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
